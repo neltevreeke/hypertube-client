6665c7f9d43a5b4040e586ca92422ffe02aa0b66
neltevreeke <nelte.p.vreeke@gmail.com>
2020-03-12 18:34:27 +0100

    changed usersapi route /login to login

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,7 @@
 import request from 'utils/request'
 
 export const login = (loginValues) => {
-  return request('/login', {
+  return request('login', {
     method: 'POST',
     body: loginValues
   })
