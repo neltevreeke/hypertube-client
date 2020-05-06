c55ebef6787fa7814bdc5eed1e50242764808dc3
neltevreeke <nelte.p.vreeke@gmail.com>
2020-05-06 14:13:56 +0200

    start on content for profile

--- src/pages/Profile/components/UserProfile/UserProfile.js
+++ src/pages/Profile/components/UserProfile/UserProfile.js
@@ -9,27 +9,53 @@ import Button from 'components/Button/Button'
 
 const UserProfile = () => {
   const user = useSelector(getUser)
+  const recentlyWatchedMovies = null
+  const recentComments = null
 
   const editProfileBtnOnClick = () => {
     history.push(Routes.PROFILE_EDIT)
   }
 
   return (
-    <div className={styles.pageHeader}>
-      <Avatar
-        size={Avatar.SIZE_M}
-        user={user}
-      />
-      <div className={styles.pageTitle}>
-        {user.username}
+    <div className={styles.component}>
+      <div className={styles.pageHeader}>
+        <Avatar
+          size={Avatar.SIZE_M}
+          user={user}
+        />
+        <div className={styles.pageTitle}>
+          {user.username}
+        </div>
+        <div className={styles.pageControls}>
+          <Button
+            variant={Button.VARIANT_DEFAULT}
+            onClick={editProfileBtnOnClick}
+          >
+            edit profile
+          </Button>
+        </div>
       </div>
-      <div className={styles.pageControls}>
-        <Button
-          variant={Button.VARIANT_DEFAULT}
-          onClick={editProfileBtnOnClick}
-        >
-          edit profile
-        </Button>
+      <div className={styles.content}>
+        <div className={styles.recentlyWatchedMovies}>
+          <p className={styles.sectionTitle}>recently watched</p>
+          {recentlyWatchedMovies ? (
+            { recentlyWatchedMovies }
+          ) : (
+            <div className={styles.empty}>
+              <p>This user has yet to watch any movies...</p>
+            </div>
+          )}
+        </div>
+        <div className={styles.recentComments}>
+          <p className={styles.sectionTitle}>recent comments</p>
+          {recentComments ? (
+            { recentComments }
+          ) : (
+            <div className={styles.empty}>
+              <p>This user has yet to comment on any movies...</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
