dd70521be9d0b1d80ee54bb42fcfbdc01df9ef7a
neltevreeke <nelte.p.vreeke@gmail.com>
2020-09-14 15:35:17 +0200

    start on moviedetails page

--- src/pages/MovieDetails/components/MovieDetails/MovieDetails.js
+++ src/pages/MovieDetails/components/MovieDetails/MovieDetails.js
@@ -8,6 +8,7 @@ import * as Routes from 'constants/Routes'
 import { getMovieDetails } from '../../../../actions/movies'
 import PageSpinner from 'components/PageSpinner/PageSpinner'
 import { getMovieDetailsIsLoading, getMovieDetailsDetails } from 'selectors/movie'
+import Button from '../../../../components/Button/Button'
 
 const MovieDetails = () => {
   const query = useSelector(getParamQueryString)
@@ -30,7 +31,28 @@ const MovieDetails = () => {
   return (
     <Page>
       <div className={styles.component}>
-        {movieDetails.title}
+        <div className={styles.heading}>
+          <img
+            src={movieDetails.medium_cover_image}
+            className={styles.image}
+          />
+          <div className={styles.contentContainer}>
+            <div className={styles.title}>
+              {movieDetails.title_english}
+            </div>
+            <div>
+              <Button variant={Button.VARIANT_DEFAULT}>
+                watch trailer
+              </Button>
+              <Button
+                className={styles.btn}
+                variant={Button.VARIANT_DEFAULT}
+              >
+                watch movie
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </Page>
   )
