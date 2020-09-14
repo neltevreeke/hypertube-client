90bfbdba588649c09634adc858c3f470fa43da9b
neltevreeke <nelte.p.vreeke@gmail.com>
2020-09-14 12:49:16 +0200

    added content and styling for moviecards

--- src/pages/Movies/components/MovieCard/MovieCard.js
+++ src/pages/Movies/components/MovieCard/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './MovieCard.scss'
+import Button from 'components/Button/Button'
 
 const MovieCard = ({
   movie
@@ -9,7 +10,24 @@ const MovieCard = ({
       style={{ backgroundImage: `url(${movie.medium_cover_image})` }}
       className={styles.component}
     >
-      {movie.title}
+      <div className={styles.content}>
+        <div className={styles.title}>
+          {movie.title}
+        </div>
+        <div className={styles.year}>
+          {movie.year}
+        </div>
+        <div className={styles.rating}>
+          <p>rating</p>
+          {movie.rating}/10
+        </div>
+        <Button
+          variant={Button.VARIANT_DEFAULT}
+          className={styles.btn}
+        >
+          movie details
+        </Button>
+      </div>
     </div>
   )
 }
